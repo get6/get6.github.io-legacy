f89fe76691beafbe7551289ccd0b11907a9c72c8
get6 <get6@users.noreply.github.com>
2021-03-15 09:10:06 +0000

    deployed via Gatsby Publish Action 🎩 for 36a20d7844b0ecccf050fac8b1e3419ff072b943

---
+++ component---src-pages-404-tsx-3a9a44cd4df2b2781fb3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),o=a.n(n),i=a("9Dj+"),l=a("9TM5");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return o.a.createElement(i.a,{location:a,title:n},o.a.createElement(l.a,{title:"404: Not Found"}),o.a.createElement("h1",null,"Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-3a9a44cd4df2b2781fb3.js.map
