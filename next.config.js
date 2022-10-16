74c053a3559074888df8742ee7db234760b700ac
get6 <ittae.com@gmail.com>
2022-10-16 23:03:52 +0900

    Prettier 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  ...withMDX({
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  }),
 }
 
 module.exports = nextConfig
---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import { NextSeo } from 'next-seo'
+
+const About = () => {
+  return (
+    <>
+      <NextSeo title="About me" description="저를 소개합니다." />
+    </>
+  )
+}
