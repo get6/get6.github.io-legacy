fc399d91442b5be4aa3e13d02cb80e4902c05c3a
get6 <ittae.com@gmail.com>
2022-10-17 12:52:32 +0900

    스타일 작업

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,35 @@
 import Link from 'next/link'
 
+type Menu = {
+  name: string
+  href: string
+}
+
 const NavBar = () => {
+  const menus: Menu[] = [
+    { name: 'Categories', href: '/categories' },
+    { name: 'Tags', href: '/tags' },
+    { name: 'Books', href: '/books' },
+    { name: 'About me', href: '/about' },
+  ]
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href={'/'}>
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href={'/about'}>
-            <a>About me</a>
-          </Link>
-        </li>
+    <nav className="absolute flex w-full justify-between py-4">
+      <div className="ml-20 mr-40 flex-none">
+        <Link href={'/'}>
+          <a className="font-bold">🌎 sunhwang&apos;s blog</a>
+        </Link>
+      </div>
+      <ul className="flex flex-auto flex-row justify-evenly">
+        {menus.map((menu, index) => (
+          <li key={index}>
+            <Link href={menu.href}>
+              <a className="hover:underline hover:underline-offset-8">
+                {menu.name}
+              </a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   )
