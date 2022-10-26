e61d4b9e3a5112feba0c75573726182711acbcaf
get6 <ittae.com@gmail.com>
2022-10-26 21:45:23 +0900

    마크다운 연결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import PostPreview from '../components/PostPreview'
+import Post from '../types/post'
+import { getAllPosts } from './api/content'
 
-const Home: NextPage = () => {
+type Props = {
+  allPosts: Post[]
+}
+
+const Home: NextPage<Props> = ({ allPosts }) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>sunhwang&apos;s blog</title>
+        <title>Sunhwang&apos;s blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -38,52 +45,9 @@ const Home: NextPage = () => {
           <hr className="my-8 border-gray-200 dark:border-gray-700" />
 
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
-            <div>
-              <Image
-                className="h-64 w-full rounded-lg object-cover object-center lg:h-80"
-                src="https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                alt=""
-                width={400}
-                height={400}
-              />
-
-              <div className="mt-8">
-                <span className="uppercase text-blue-500">category</span>
-
-                <h1 className="mt-4 text-xl font-semibold text-gray-800 dark:text-white">
-                  What do you want to know about UI
-                </h1>
-
-                <p className="mt-2 text-gray-500 dark:text-gray-400">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-                  est asperiores vel, ab animi recusandae nulla veritatis id
-                  tempore sapiente
-                </p>
-
-                <div className="mt-4 flex items-center justify-between">
-                  <div>
-                    <a
-                      href="#"
-                      className="text-lg font-medium text-gray-700 hover:text-gray-500 hover:underline dark:text-gray-300"
-                    >
-                      John snow
-                    </a>
-
-                    <p className="text-sm text-gray-500 dark:text-gray-400">
-                      February 1, 2022
-                    </p>
-                  </div>
-
-                  <a
-                    href="#"
-                    className="inline-block text-blue-500 underline hover:text-blue-400"
-                  >
-                    Read more
-                  </a>
-                </div>
-              </div>
-            </div>
-
+            {allPosts.map((post, index) => (
+              <PostPreview key={index} {...post} />
+            ))}
             <div>
               <Image
                 className="h-64 w-full rounded-lg object-cover object-center lg:h-80"
@@ -195,3 +159,18 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
